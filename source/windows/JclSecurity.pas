ef17f2f6fa0fe60622acf3bf0a61ff4bab7a8262
mthoma <mthoma@9a6cfd80-1415-5440-b600-b8f113e22350>
2000-07-17 00:39:50 +0000

    Filename: jclsecurity.pas Path: v:\code library\source\windows\ Description: - Added to archive: 7/26/00 3:19:05 PM RevisionID: 61 Version: 0 Revision: 1 Author: Matthias Thoma Checkout comment: Checkin comment: Timestamp: 17/07/2000 00:39:50 Size: 8485

---
+++ source/windows/JclSecurity.pas
@@ -0,0 +1,238 @@
+{******************************************************************************}
+{                                                                              }
+{ Project JEDI Code Library (JCL)                                              }
+{                                                                              }
+{ The contents of this file are subject to the Mozilla Public License Version  }
+{ 1.0 (the "License"); you may not use this file except in compliance with the }
+{ License. You may obtain a copy of the License at http://www.mozilla.org/MPL/ }
+{                                                                              }
+{ Software distributed under the License is distributed on an "AS IS" basis,   }
+{ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for }
+{ the specific language governing rights and limitations under the License.    }
+{                                                                              }
+{ The Original Code is JclSecurity.pas.                                        }
+{                                                                              }
+{ The Initial Developer of the Original Code is documented in the accompanying }
+{ help file JCL.chm. Portions created by these individuals are Copyright (C)   }
+{ of these individuals.                                                        }
+{                                                                              }
+{ Last modified: April 21, 2000                                                }
+{                                                                              }
+{******************************************************************************}
+
+unit JclSecurity;
+
+{$I JCL.INC}
+
+{$WEAKPACKAGEUNIT ON}
+
+interface
+
+uses
+  {$IFDEF WIN32}
+  Windows,
+  {$ENDIF}
+  JclBase;
+
+//------------------------------------------------------------------------------
+// Access Control
+//------------------------------------------------------------------------------
+
+function AllowRegKeyForEveryone(Key: HKEY; Path: string): Boolean;
+function CreateNullDacl(var Sa: TSecurityAttributes;
+  const Inheritable: Boolean): PSecurityAttributes;
+function CreateInheritable(var Sa: TSecurityAttributes): PSecurityAttributes;
+
+//------------------------------------------------------------------------------
+// Privileges
+//------------------------------------------------------------------------------
+
+function IsAdministrator: Boolean;
+function EnableProcessPrivilege(const Enable: Boolean;
+  const Privilege: string): Boolean;
+function EnableThreadPrivilege(const Enable: Boolean;
+  const Privilege: string): Boolean;
+function PrivilegeEnabled(const Privilege: string): Boolean;
+
+implementation
+
+uses
+  {$IFDEF COMPILER5_UP}
+  AccCtrl, AclApi,
+  {$ENDIF}
+  SysUtils,
+  JclWin32;
+
+//==============================================================================
+// Access Control
+//==============================================================================
+
+function AllowRegKeyForEveryone(Key: HKEY; Path: string): Boolean;
+var
+  WidePath: PWideChar;
+  Len: Integer;
+begin
+  case Key of
+    HKEY_LOCAL_MACHINE:
+      Path := 'MACHINE\' + Path;
+    HKEY_CURRENT_USER:
+      Path := 'CURRENT_USER\' + Path;
+    HKEY_CLASSES_ROOT:
+      Path := 'CLASSES_ROOT\' + Path;
+    HKEY_USERS:
+      Path := 'USERS\' + Path;
+  end;
+  Len := (Length(Path)+1)*SizeOf(WideChar);
+  GetMem(WidePath,Len);
+  MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, PChar(Path), -1, WidePath, Len);
+  Result := SetNamedSecurityInfoW(WidePath, SE_REGISTRY_KEY,
+    DACL_SECURITY_INFORMATION, nil, nil, nil, nil) = ERROR_SUCCESS;
+  FreeMem(WidePath);
+end;
+
+//------------------------------------------------------------------------------
+
+function CreateNullDacl(var Sa: TSecurityAttributes;
+  const Inheritable: Boolean): PSecurityAttributes;
+var
+  Sd: PSecurityDescriptor;
+begin
+  Sd := AllocMem(SizeOf(Sd));
+  InitializeSecurityDescriptor(Sd, SECURITY_DESCRIPTOR_REVISION);
+  SetSecurityDescriptorDacl(Sd, True, nil, False);
+  Sa.nLength := SizeOf(Sa);
+  Sa.lpSecurityDescriptor := @Sd;
+  Sa.bInheritHandle := Inheritable;
+  Result := @Sa;
+end;
+
+//------------------------------------------------------------------------------
+
+function CreateInheritable(var Sa: TSecurityAttributes): PSecurityAttributes;
+begin
+  Sa.nLength := SizeOf(Sa);
+  Sa.lpSecurityDescriptor := nil;
+  Sa.bInheritHandle := True;
+  Result := @Sa;
+end;
+
+//==============================================================================
+// Privileges
+//==============================================================================
+
+function IsAdministrator: Boolean;
+var
+  psidAdmin: Pointer;
+  Token: THandle;
+  Count: DWORD;
+  TokenInfo: PTokenGroups;
+  HaveToken: Boolean;
+  I: Integer;
+begin
+  Result := False;
+  psidAdmin := nil;
+  HaveToken := OpenThreadToken(GetCurrentThread, TOKEN_QUERY, True, Token);
+  if (not HaveToken) and (GetLastError = ERROR_NO_TOKEN) then
+  begin
+    HaveToken := OpenProcessToken(GetCurrentProcess, TOKEN_QUERY, Token);
+  end;
+  if HaveToken then
+  begin
+    AllocateAndInitializeSid(SECURITY_NT_AUTHORITY, 2,
+      SECURITY_BUILTIN_DOMAIN_RID, DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, 0, 0, 0,
+      psidAdmin);
+    GetTokenInformation(Token, TokenGroups, nil, 0, Count);
+    TokenInfo := PTokenGroups(AllocMem(Count));
+    GetTokenInformation(Token, TokenGroups, TokenInfo, Count, Count);
+    for I := 0 to TokenInfo^.GroupCount - 1 do
+    begin
+      {$R-} // Groups is an array [0..0] of TSIDAndAttributes, ignore ERangeError
+      Result := EqualSid(psidAdmin, TokenInfo^.Groups[I].Sid);
+      {$R+}
+      if Result then
+        Break;
+    end;
+    FreeMem(TokenInfo);
+    CloseHandle(Token);
+    FreeSid(psidAdmin);
+  end;
+end;
+
+//------------------------------------------------------------------------------
+
+function EnableProcessPrivilege(const Enable: Boolean;
+  const Privilege: string): Boolean;
+const
+  PrivAttrs: array [Boolean] of DWORD = (0, SE_PRIVILEGE_ENABLED);
+var
+  Token: THandle;
+  TokenPriv: TTokenPrivileges;
+begin
+  Result := False;
+  if OpenProcessToken(GetCurrentProcess, TOKEN_ADJUST_PRIVILEGES, Token) then
+  begin
+    TokenPriv.PrivilegeCount := 1;
+    LookupPrivilegeValue(nil, PChar(Privilege), TokenPriv.Privileges[0].Luid);
+    TokenPriv.Privileges[0].Attributes := PrivAttrs[Enable];
+    JclWin32.AdjustTokenPrivileges(Token, False, TokenPriv, SizeOf(TokenPriv),
+      nil, nil);
+    Result := GetLastError = ERROR_SUCCESS;
+    CloseHandle(Token);
+  end;
+end;
+
+//------------------------------------------------------------------------------
+
+function EnableThreadPrivilege(const Enable: Boolean;
+  const Privilege: string): Boolean;
+const
+  PrivAttrs: array [Boolean] of DWORD = (0, SE_PRIVILEGE_ENABLED);
+var
+  Token: THandle;
+  TokenPriv: TTokenPrivileges;
+  HaveToken: Boolean;
+begin
+  Result := False;
+  Token := 0;
+  HaveToken := OpenThreadToken(GetCurrentThread, TOKEN_ADJUST_PRIVILEGES,
+    False, Token);
+  if (not HaveToken) and (GetLastError = ERROR_NO_TOKEN) then
+    HaveToken := OpenProcessToken(GetCurrentProcess, TOKEN_ADJUST_PRIVILEGES,
+      Token);
+  if HaveToken then
+  begin
+    TokenPriv.PrivilegeCount := 1;
+    LookupPrivilegeValue(nil, PChar(Privilege), TokenPriv.Privileges[0].Luid);
+    TokenPriv.Privileges[0].Attributes := PrivAttrs[Enable];
+    JclWin32.AdjustTokenPrivileges(Token, False, TokenPriv, SizeOf(TokenPriv),
+      nil, nil);
+    Result := GetLastError = ERROR_SUCCESS;
+    CloseHandle(Token);
+  end;
+end;
+
+//------------------------------------------------------------------------------
+
+function PrivilegeEnabled(const Privilege: string): Boolean;
+var
+  Token: THandle;
+  TokenPriv: TPrivilegeSet;
+  Res: LongBool;
+  HaveToken: Boolean;
+begin
+  Result := False;
+  Token := 0;
+  HaveToken := OpenThreadToken(GetCurrentThread, TOKEN_QUERY, False, Token);
+  if (not HaveToken) and (GetLastError = ERROR_NO_TOKEN) then
+    HaveToken := OpenProcessToken(GetCurrentProcess, TOKEN_QUERY, Token);
+  if HaveToken then
+  begin
+    TokenPriv.PrivilegeCount := 1;
+    TokenPriv.Control := 0;
+    LookupPrivilegeValue(nil, PChar(Privilege), TokenPriv.Privilege[0].Luid);
+    Result := PrivilegeCheck(Token, TokenPriv, Res) and Res;
+    CloseHandle(Token);
+  end;
+end;
+
+end.
