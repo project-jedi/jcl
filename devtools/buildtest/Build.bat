11a79f1ff45d0dca420b6b60ddc0e5a51bc3828d
pvones <pvones@9a6cfd80-1415-5440-b600-b8f113e22350>
2003-04-01 13:46:08 +0000

    Filename: build.bat Path: v:\code library\devtools\buildtest\ Description: - Added to archive: 4/1/2003 1:45:12 PM RevisionID: 5899 Version: 0 Revision: 1 Author: Petr Vones Checkout comment: Checkin comment: Timestamp: 01/04/2003 13:46:08 Size: 210

---
+++ devtools/buildtest/Build.bat
@@ -0,0 +1,5 @@
+"%ProgramFiles%\Borland\Delphi5\Bin\dcc32.exe" BuildTest.dpr
+"%ProgramFiles%\Borland\Delphi6\Bin\dcc32.exe" BuildTest.dpr
+"%ProgramFiles%\Borland\Delphi7\Bin\dcc32.exe" BuildTest.dpr
+del BuildTest.exe
+pause
