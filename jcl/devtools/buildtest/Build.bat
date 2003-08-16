158e676c9bebe142464336d21a8ed804da4aa9a5
pvones <pvones@9a6cfd80-1415-5440-b600-b8f113e22350>
2003-08-16 18:23:52 +0000

    RevisionID: 6142 Version: 0 Revision: 2 Author: Petr Vones Checkout comment: Checkin comment: Timestamp: 16/08/2003 18:23:52 Size: 301

--- jcl/devtools/buildtest/Build.bat
+++ jcl/devtools/buildtest/Build.bat
@@ -1,5 +1,13 @@
 "%ProgramFiles%\Borland\Delphi5\Bin\dcc32.exe" BuildTest.dpr
+pause Delphi 5 compiler
+cls
+
 "%ProgramFiles%\Borland\Delphi6\Bin\dcc32.exe" BuildTest.dpr
+pause Delphi 6 compiler
+cls
+
 "%ProgramFiles%\Borland\Delphi7\Bin\dcc32.exe" BuildTest.dpr
+pause Delphi 7 compiler
+cls
+
 del BuildTest.exe
-pause
