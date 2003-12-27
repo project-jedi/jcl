3fccbf95b09ce93912a3379b9c2ecb6861d25913
rrossmair <rrossmair@9a6cfd80-1415-5440-b600-b8f113e22350>
2003-12-27 04:40:57 +0000

    added JclTask to "uses" clause

--- jcl/examples/vcl/tasks/TaskDemo.dpr
+++ jcl/examples/vcl/tasks/TaskDemo.dpr
@@ -2,6 +2,7 @@ program TaskDemo;
 
 uses
   Forms,
+  JclTask in '..\..\..\source\windows\JclTask.pas',
   TaskDemoMain in 'TaskDemoMain.pas' {frmMain},
   TaskDemoDataModule in 'TaskDemoDataModule.pas' {DM: TDataModule};
 
