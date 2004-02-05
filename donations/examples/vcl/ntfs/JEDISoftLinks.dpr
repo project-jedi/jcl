044698cb7e18c1cc91a2f502693b6e391aef2ad0
rrossmair <rrossmair@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-02-05 23:24:47 +0000

    initial check in

---
+++ donations/examples/vcl/ntfs/JEDISoftLinks.dpr
@@ -0,0 +1,16 @@
+library JEDISoftLinks;
+
+{$R JEDISoftLinks.tlb}
+
+uses
+  ComServ,
+  SoftLinkDragDropHandler in 'SoftLinkDragDropHandler.pas';
+
+exports
+  DllGetClassObject,
+  DllCanUnloadNow,
+  DllRegisterServer,
+  DllUnregisterServer;
+
+begin
+end.
