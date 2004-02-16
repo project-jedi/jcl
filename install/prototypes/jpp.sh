d758cdb398a3259ce98bcff996794258e84800d7
rrossmair <rrossmair@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-02-16 05:00:55 +0000

    initial check-in

---
+++ install/prototypes/jpp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# shell script to generate installer units from prototypes
+#
+# Robert Rossmair, 2004-02-16
+#
+
+JPP=../../source/prototypes/jpp
+OPTIONS="-c -dVisualCLX -dHAS_UNIT_TYPES -uDevelop -uVCL -x../Q"
+FILES="JclInstall.pas JediInstallIntf.pas ProductFrames.pas JediInstallerMain.pas JediInstaller.dpr"
+
+$JPP $OPTIONS $FILES
+mv ../QJediInstaller.pas ../QJediInstaller.dpr
