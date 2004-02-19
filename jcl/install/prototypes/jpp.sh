beca9aa697570fd7477dfa0ac47ffe80e11e85f2
rrossmair <rrossmair@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-02-19 07:13:10 +0000

    now checks first if ..\QJediInstaller.pas exists before it tries to rename it.

--- jcl/install/prototypes/jpp.sh
+++ jcl/install/prototypes/jpp.sh
@@ -10,4 +10,6 @@ OPTIONS="-c -dVisualCLX -dHAS_UNIT_TYPES -uDevelop -uVCL -x../Q"
 FILES="JclInstall.pas JediInstallIntf.pas ProductFrames.pas JediInstallerMain.pas JediInstaller.dpr"
 
 $JPP $OPTIONS $FILES
-mv ../QJediInstaller.pas ../QJediInstaller.dpr
+if [ -f ../QJediInstaller.pas ]; then
+  mv ../QJediInstaller.pas ../QJediInstaller.dpr
+fi
