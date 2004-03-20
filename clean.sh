65a60d827cab087cc493a2846249d0fd1dc7829f
rrossmair <rrossmair@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-03-20 18:02:56 +0000

    initial check-in

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd $JCL
+rm -f `find -name *.~*`
+rm -f `find -name *.a`
+rm -f `find -name *.bpi`
+rm -f `find -name *.dcp`
+rm -f `find -name *.dcu`
+rm -f `find -name *.dpu`
+rm -f `find -name *.hpp`
+rm -f `find -name *.o`
