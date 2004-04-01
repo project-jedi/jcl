9ad3eee8204ebbc8ae47e0a4748383485a393945
rrossmair <rrossmair@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-04-01 00:06:23 +0000

    updated

--- install/prototypes/jpp.sh
+++ install/prototypes/jpp.sh
@@ -4,12 +4,11 @@
 #
 # Robert Rossmair, 2004-02-16
 #
+# $Id$
 
 JPP=../../source/prototypes/jpp
 OPTIONS="-c -dVisualCLX -dHAS_UNIT_TYPES -uDevelop -uVCL -x../Q"
-FILES="JclInstall.pas JediInstallIntf.pas ProductFrames.pas JediInstallerMain.pas JediInstaller.dpr"
+FILES="JclInstall.pas JediInstallIntf.pas ProductFrames.pas JediInstallerMain.pas"
 
 $JPP $OPTIONS $FILES
-if [ -f ../QJediInstaller.pas ]; then
-  mv ../QJediInstaller.pas ../QJediInstaller.dpr
-fi
+
