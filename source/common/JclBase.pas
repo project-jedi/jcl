51ac28bd2898f07043063a3b53a396793414dba2
rrossmair <rrossmair@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-04-06 00:43:28 +0000

    JclVersionBuild updated

--- source/common/JclBase.pas
+++ source/common/JclBase.pas
@@ -50,7 +50,7 @@ const
   JclVersionMajor   = 1;    // 0=pre-release|beta/1, 2, ...=final
   JclVersionMinor   = 90;   // Forth minor release JCL 1.20
   JclVersionRelease = 0;    // 0=pre-release|beta/1=release
-  JclVersionBuild   = 1400;  // build number, days since march 1, 2000
+  JclVersionBuild   = 1497; // build number, days since march 1, 2000
   JclVersion = (JclVersionMajor shl 24) or (JclVersionMinor shl 16) or
     (JclVersionRelease shl 15) or (JclVersionBuild shl 0);
 
