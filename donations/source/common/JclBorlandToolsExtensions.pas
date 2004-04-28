7b7d68bc63571d49dae552e3972e0680e8b29bed
uschuster <uschuster@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-04-28 20:16:06 +0000

    added TJclCustomDCCConfig, TJclDCCConfigFile and TJclDOFFile; added options BuildAllUnits and QuietCompile to TJclDCCEx

--- donations/source/common/JclBorlandToolsExtensions.pas
+++ donations/source/common/JclBorlandToolsExtensions.pas
@@ -24,7 +24,7 @@
 {   into JclBorlandTools.pas.                                                                      }
 {                                                                                                  }
 { Unit owner: Uwe Schuster                                                                         }
-{ Last modified: April 07, 2004                                                                    }
+{ Last modified: April 11, 2004                                                                    }
 {                                                                                                  }
 {**************************************************************************************************}
 
@@ -32,7 +32,7 @@
 Todo:
 - style:
   - sort class members
-  - insert markers 
+  - insert markers
 - support Aborted and set current dir in ...ExecAndCapture
 - IFDEF OS specific compiler switches and defines
 - get settings from .cfg or .dof
@@ -54,11 +54,71 @@ uses
   {$IFDEF LINUX}
   Libc, Types,
   {$ENDIF}
-  SysUtils, Classes;
+  SysUtils, Classes, IniFiles;
 
 type
   TJclDCCMessageKind = (mkUnknown, mkHint, mkWarning, mkError, mkFatal);
   TJclDCCMapFileLevel = (mfloff, mflsegments, mflpublics, mfldetailed);
+
+  //(usc) need optionen "MakeModifiedUnits" (-M) ?
+  //   I guess -M is opposite of -B  
+  TJclCustomDCCConfig = class(TObject)
+  private
+    FBPLOutputDirectory: string;
+    FBuildAllUnits: Boolean;
+    FConditionalDefines: string;
+    FConsoleApplication: Boolean;
+    FDCPOutputDirectory: string;
+    FDCUOutputDir: string;
+    FEXEOutputDir: string;
+    FImageBaseAddr: DWord;
+    FIncludeDirectories: string;
+    FMapFileLevel: TJclDCCMapFileLevel;
+    FMaxStackSize: Integer;
+    FMinStackSize: Integer;
+    FObjectDirectories: string;
+    FOutputHints: Boolean;
+    FOutputWarnings: Boolean;
+    FPackages: string;
+    FResourceDirectories: string;
+    FUnitAliases: string;
+    FUnitDirectories: string;
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    property BPLOutputDirectory: string read FBPLOutputDirectory write FBPLOutputDirectory;
+    property BuildAllUnits: Boolean read FBuildAllUnits write FBuildAllUnits;
+    //(usc) TJclDCCEx name is Defines only
+    property ConditionalDefines: string read FConditionalDefines write FConditionalDefines;
+    property ConsoleApplication: Boolean read FConsoleApplication write FConsoleApplication;
+    property DCPOutputDirectory: string read FDCPOutputDirectory write FDCPOutputDirectory;
+    //(usc) DCU & EXE dir are named DCU/EXEOutputDir in TJclDCCEx!
+    property DCUOutputDirectory: string read FDCUOutputDir write FDCUOutputDir;
+    property EXEOutputDirectory: string read FEXEOutputDir write FEXEOutputDir;
+    property ImageBaseAddr: DWord read FImageBaseAddr write FImageBaseAddr;
+    property IncludeDirectories: string read FIncludeDirectories write FIncludeDirectories;
+    property MapFileLevel: TJclDCCMapFileLevel read FMapFileLevel write FMapFileLevel;    
+    property MaxStackSize: Integer read FMaxStackSize write FMaxStackSize;
+    property MinStackSize: Integer read FMinStackSize write FMinStackSize;
+    property ObjectDirectories: string read FObjectDirectories write FObjectDirectories;
+    property OutputHints: Boolean read FOutputHints write FOutputHints;
+    property OutputWarnings: Boolean read FOutputWarnings write FOutputWarnings;
+    property Packages: string read FPackages write FPackages;
+    property ResourceDirectories: string read FResourceDirectories write FResourceDirectories;
+    property UnitAliases: string read FUnitAliases write FUnitAliases;
+    property UnitDirectories: string read FUnitDirectories write FUnitDirectories;
+  end;
+
+  TJclDCCConfigFile = class(TJclCustomDCCConfig)
+  public
+    procedure LoadFromFile(AFileName: string);
+  end;
+
+  TJclDOFFile = class(TJclCustomDCCConfig)
+  public
+    procedure LoadFromFile(AFileName: string);
+  end;
 
   TJclDCCEx = class(TObject)
   private
@@ -91,6 +151,8 @@ type
     FMaxStackSize: DWord;
     FImageBase: DWord;
     FOnCompileUpdate: TNotifyEvent;
+    FBuildAllUnits: Boolean;
+    FQuietCompile: Boolean;
     procedure CaptureLine(const Line: string; var Aborted: Boolean);
     procedure ClearValues;
   public
@@ -99,6 +161,8 @@ type
 
     function Compile: Boolean;
 
+    property BuildAllUnits: Boolean read FBuildAllUnits write FBuildAllUnits;
+    property QuietCompile: Boolean read FQuietCompile write FQuietCompile;
     property ExeName: string read FExeName write FExeName;
     property CurrentFile: string read FCurrentFile;
     property CurrentLineNo: Integer read FCurrentLineNo;
@@ -136,10 +200,231 @@ type
 
 implementation
 
-{$IFDEF DELPHI5}
 uses
-  FileCtrl;
-{$ENDIF DELPHI5}
+  {$IFDEF DELPHI5}
+  FileCtrl,
+  {$ENDIF DELPHI5}
+  JclStrings;
+
+constructor TJclCustomDCCConfig.Create;
+begin
+  inherited Create;
+  FBPLOutputDirectory := '';
+  FBuildAllUnits := True;
+  FConditionalDefines := '';
+  FConsoleApplication := False;
+  FDCPOutputDirectory := '';
+  FDCUOutputDir:= '';
+  FEXEOutputDir:= '';
+  FImageBaseAddr := $400000;
+  FIncludeDirectories := '';
+  FMapFileLevel := mfloff;
+  FMaxStackSize := 1048576;
+  FMinStackSize := 16384;
+  FObjectDirectories := '';
+  FOutputHints := True;
+  FOutputWarnings := True;
+  FPackages := '';
+  FResourceDirectories := '';
+  FUnitAliases := '';
+  FUnitDirectories := '';
+end;
+
+destructor TJclCustomDCCConfig.Destroy;
+begin
+  inherited Destroy;
+end;
+
+procedure TJclDCCConfigFile.LoadFromFile(AFileName: string);
+
+  function CheckPathOption(const AOptionPrefix: string; AOption: string; var ADestPath: string): Boolean;
+  begin
+    Result := Pos(AOptionPrefix, AOption) = 1;
+    if Result then
+    begin
+      Delete(AOption, 1, Length(AOptionPrefix));
+      ADestPath := StrTrimQuotes(AOption);
+    end;
+  end;
+
+var
+  ConfigStrings: TStringList;
+  I: Integer;
+  S, S2: string;
+begin
+  ConfigStrings := TStringList.Create;
+  try
+    //(usc) do FileExists check ?
+    ConfigStrings.LoadFromFile(AFileName);
+    MapFileLevel := mfloff; //(usc) set to off ?
+    for I := 0 to Pred(ConfigStrings.Count) do
+    begin
+      S := ConfigStrings[I];
+      if Pos('-GS', S) = 1 then
+        MapFileLevel := mflsegments
+      else
+      if Pos('-GP', S) = 1 then
+        MapFileLevel := mflpublics
+      else
+      if Pos('-GD', S) = 1 then
+        MapFileLevel := mfldetailed
+      else
+
+      // JPNE (linker output)
+
+      if Pos('-cc', S) = 1 then
+        ConsoleApplication := True
+      else
+      if Pos('-cg', S) = 1 then
+        ConsoleApplication := False
+      else
+
+      // VN (TD32 debug info)
+      // VR (remote debug symbols)
+
+      if Pos('-A', S) = 1 then
+      begin
+        Delete(S, 1, 2);
+        UnitAliases := S;
+      end
+      else
+      if Pos('-H', S) = 1 then
+        OutputHints := Pos('+', S) = 3
+      else
+      if Pos('-W', S) = 1 then
+        OutputWarnings := Pos('+', S) = 3
+      else
+      if Pos('-M', S) = 1 then
+        BuildAllUnits := False
+      else
+      if Pos('-B', S) = 1 then //(usc) not found in .cfg file - but used as opposite of -M ?
+        BuildAllUnits := True
+      else
+      if (Pos('-$M', S) = 1) and (Pos('-$M-', S) = 0) and (Pos('-$M+', S) = 0) and
+        (Pos(',', S) > 3) then
+      begin
+        Delete(S, 1, 3);
+        S2 := Copy(S, 1, Pos(',', S) - 1);
+        MinStackSize := StrToIntDef(S2, MinStackSize);
+        S2 := S;
+        Delete(S2, 1, Pos(',', S));
+        MaxStackSize := StrToIntDef(S2, MaxStackSize);
+      end
+      else
+      if Pos('-K', S) = 1 then
+      begin
+        Delete(S, 1, 2);
+        ImageBaseAddr := StrToIntDef(S, ImageBaseAddr);
+      end
+      else
+      if CheckPathOption('-E', S, S2) then
+        EXEOutputDirectory := S2
+      else
+      if CheckPathOption('-N', S, S2) then
+        DCUOutputDirectory := S2
+      else
+      if CheckPathOption('-LE', S, S2) then
+        BPLOutputDirectory := S2
+      else
+      if CheckPathOption('-LN', S, S2) then
+        DCPOutputDirectory := S2
+      else
+      if CheckPathOption('-U', S, S2) then
+        UnitDirectories := S2
+      else
+      if CheckPathOption('-O', S, S2) then
+        ObjectDirectories := S2
+      else
+      if CheckPathOption('-I', S, S2) then
+        IncludeDirectories := S2
+      else
+      if CheckPathOption('-R', S, S2) then
+        ResourceDirectories := S2
+      else
+      if Pos('-D', S) = 1 then
+      begin
+        Delete(S, 1, 2);
+        ConditionalDefines := S;
+      end
+      else
+      if Pos('-LU', S) = 1 then
+      begin
+        Delete(S, 1, 3);
+        Packages := S;
+      end;
+    end;
+  finally
+    ConfigStrings.Free;
+  end;
+end;
+
+procedure TJclDOFFile.LoadFromFile(AFileName: string);
+
+  function CheckPathOption(const AOptionPrefix: string; AOption: string; var ADestPath: string): Boolean;
+  begin
+    Result := Pos(AOptionPrefix, AOption) = 1;
+    if Result then
+    begin
+      Delete(AOption, 1, Length(AOptionPrefix));
+      ADestPath := StrTrimQuotes(AOption);
+    end;
+  end;
+
+const
+  CompilerSection = 'Compiler';
+  LinkerSection = 'Linker';
+  DirectoriesSection = 'Directories';
+
+var
+  DOFFile: TIniFile;
+  SearchPath: string;
+begin
+  //(usc) do FileExists check ?
+  DOFFile := TIniFile.Create(AFileName);
+  try
+    //todo Usepackages
+    case DOFFile.ReadInteger(LinkerSection, 'MapFile', 0) of
+      0: MapFileLevel := mfloff;
+      1: MapFileLevel := mflsegments;
+      2: MapFileLevel := mflpublics;
+      3: MapFileLevel := mfldetailed;
+    end;
+    // JPNE (linker output)
+    ConsoleApplication := DOFFile.ReadInteger(LinkerSection, 'ConsoleApp', 0) = 1;
+    // VN (TD32 debug info)
+    // VR (remote debug symbols)
+    UnitAliases := DOFFile.ReadString(CompilerSection, 'UnitAliases', '');
+    OutputHints := DOFFile.ReadInteger(CompilerSection, 'ShowHints', 0) = 1;
+    OutputWarnings := DOFFile.ReadInteger(CompilerSection, 'ShowWarnings', 0) = 1;
+{
+      if Pos('-M', S) = 1 then
+        FBuildAllUnits := False
+      else
+      if Pos('-B', S) = 1 then //(usc) not found in .cfg file - but used as opposite of -M ?
+        FBuildAllUnits := True
+      else
+}
+    MinStackSize := DOFFile.ReadInteger(LinkerSection, 'MinStackSize', MinStackSize);
+    MaxStackSize := DOFFile.ReadInteger(LinkerSection, 'MaxStackSize', MaxStackSize);
+    ImageBaseAddr := DOFFile.ReadInteger(LinkerSection, 'ImageBase', ImageBaseAddr);
+
+    EXEOutputDirectory := DOFFile.ReadString(DirectoriesSection, 'OutputDir', '');
+    DCUOutputDirectory := DOFFile.ReadString(DirectoriesSection, 'UnitOutputDir', '');
+    BPLOutputDirectory := DOFFile.ReadString(DirectoriesSection, 'PackageDLLOutputDir', '');
+    DCPOutputDirectory := DOFFile.ReadString(DirectoriesSection, 'PackageDCPOutputDir', '');
+
+    SearchPath := DOFFile.ReadString(DirectoriesSection, 'SearchPath', '');
+    UnitDirectories := SearchPath;
+    ObjectDirectories := SearchPath;
+    IncludeDirectories := SearchPath;
+    ResourceDirectories := SearchPath;
+
+    ConditionalDefines := DOFFile.ReadString(DirectoriesSection, 'Conditionals', '');
+    Packages := DOFFile.ReadString(DirectoriesSection, 'Packages', '');
+  finally
+    DOFFile.Free;
+  end;
+end;
 
 type
   TCaptureLine = procedure(const ALine: string; var Aborted: Boolean) of object;
@@ -360,6 +645,8 @@ begin
   FMaxStackSize := $100000;
   FImageBase := $400000;
   FOnCompileUpdate := nil;
+  FBuildAllUnits := True;
+  FQuietCompile := False;
 end;
 
 destructor TJclDCCEx.Destroy;
@@ -441,6 +728,16 @@ function TJclDCCEx.Compile: Boolean;
 var
   Arguments: string;
 begin
+{
+ open arguments
+ - F
+ - J
+ - JP
+ - LE (not listed in dcc help)
+ - LN (not listed in dcc help)
+ - P
+ - Z
+}
   ClearValues;
   Result := False;
   if FileExists(FFileToCompile) and (FExeName <> '') then
@@ -450,6 +747,7 @@ begin
       Arguments := Arguments + Format(' -E%s', [FEXEOutputDir]);
     if DirectoryExists(FDCUOutputDir) then
       Arguments := Arguments + Format(' -N%s', [FDCUOutputDir]);
+    Arguments := Arguments + Format(' -O%s', [FSearchPaths]);      
     Arguments := Arguments + Format(' -I%s', [FSearchPaths]);
     Arguments := Arguments + Format(' -R%s', [FSearchPaths]);
     Arguments := Arguments + Format(' -U%s', [FSearchPaths]);
@@ -462,7 +760,10 @@ begin
       mflpublics: Arguments := Arguments + ' -GP';
       mfldetailed: Arguments := Arguments + ' -GD';
     end;
-    Arguments := Arguments + ' -B';  //(usc) make option
+    if FBuildAllUnits then
+      Arguments := Arguments + ' -B'
+    else
+      Arguments := Arguments + ' -M';  //(usc) is this necessary ?
     Arguments := Arguments + ' ' + FCompilerSwitches;
     if FUnitAliases <> '' then
       Arguments := Arguments + ' -A' + FUnitAliases;
@@ -485,9 +786,15 @@ begin
     //value must be hexadecimal
     Arguments := Arguments + Format(' -K%.8x', [FImageBase]);
 
-    ExecAndCapture(FExeName + ' ' + Arguments, CaptureLine);
-    Result := (FErrorCount = 0) and (FFatalCount = 0);
+    if FQuietCompile then
+      Arguments := Arguments + ' -Q';
+
+    Result := (ExecAndCapture(FExeName + ' ' + Arguments, CaptureLine) = 0) and
+      (FErrorCount = 0) and (FFatalCount = 0);
   end;
 end;
 
 end.
+
+
+
