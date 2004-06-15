127b229c121ff2eb7995a5d91c02c54134ad4dc1
rayspostbox3 <rayspostbox3@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-06-15 02:23:35 +0000

    Updated note in header.

--- source/common/JclEDI_UNEDIFACT_Ext.pas
+++ source/common/JclEDI_UNEDIFACT_Ext.pas
@@ -28,7 +28,7 @@
 { Date created: March 26, 2004                                                                     }
 { Additional Info:                                                                                 }
 {   E-Mail at RaysDelphiBox3 att hotmail dott com                                                  }
-{   For latest EDI specific updates see http://sourceforge.net/projects/edisdk                     }
+{   For latest EDI specific demos see http://sourceforge.net/projects/edisdk                       }
 {   See home page for latest news & events and online help.                                        }
 {                                                                                                  }
 {**************************************************************************************************}
