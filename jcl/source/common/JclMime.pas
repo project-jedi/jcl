3205dbae5efd6b703466f0ea00344eb7d32e1d16
mthoma <mthoma@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-07-07 22:34:25 +0000

    Added Ralf MimeStreams and MimeFile utilities...

--- jcl/source/common/JclMime.pas
+++ jcl/source/common/JclMime.pas
@@ -68,12 +68,22 @@ function MimeDecodePartial(const InputBuffer; const InputBytesCount: Cardinal;
   out OutputBuffer; var ByteBuffer: Cardinal; var ByteBufferSpace: Cardinal): Cardinal;
 function MimeDecodePartialEnd(out OutputBuffer; const ByteBuffer: Cardinal;
   const ByteBufferSpace: Cardinal): Cardinal;
+procedure MimeEncodeFile(const InputFileName, OutputFileName: AnsiString);
+procedure MimeEncodeFileNoCRLF(const InputFileName, OutputFileName: AnsiString);
+procedure MimeDecodeFile(const InputFileName, OutputFileName: AnsiString);
+procedure MimeEncodeStream(const InputStream: TStream; const OutputStream: TStream);
+procedure MimeEncodeStreamNoCRLF(const InputStream: TStream; const OutputStream: TStream);
+procedure MimeDecodeStream(const InputStream: TStream; const OutputStream: TStream);
+
 
 const
   MIME_ENCODED_LINE_BREAK = 76;
   MIME_DECODED_LINE_BREAK = MIME_ENCODED_LINE_BREAK div 4 * 3;
+  MIME_BUFFER_SIZE = MIME_DECODED_LINE_BREAK * 3 * 4 * 4;
 
 implementation
+uses
+  SysUtils;
 
 // Caution: For MimeEncodeStream and all other kinds of multi-buffered
 // Mime encodings (i.e. Files etc.), BufferSize must be set to a multiple of 3.
@@ -210,43 +220,6 @@ end;
 
 //--------------------------------------------------------------------------------------------------
 
-procedure MimeEncodeStream(const InputStream: TStream; const OutputStream: TStream);
-var
-  InputBuffer: array [0..BUFFER_SIZE - 1] of Byte;
-  OutputBuffer: array [0..((BUFFER_SIZE + 2) div 3) * 4 - 1] of Byte;
-  BytesRead: Integer;
-begin
-  BytesRead := InputStream.Read(InputBuffer, SizeOf(InputBuffer));
-  while BytesRead > 0 do
-  begin
-    MimeEncode(InputBuffer, BytesRead, OutputBuffer);
-    OutputStream.Write(OutputBuffer, MimeEncodedSize(BytesRead));
-    BytesRead := InputStream.Read(InputBuffer, SizeOf(InputBuffer));
-  end;
-end;
-
-//--------------------------------------------------------------------------------------------------
-
-procedure MimeDecodeStream(const InputStream: TStream; const OutputStream: TStream);
-var
-  ByteBuffer, ByteBufferSpace: Cardinal;
-  InputBuffer: array [0..(BUFFER_SIZE + 3) div 4 * 3 - 1] of Byte;
-  OutputBuffer: array [0..BUFFER_SIZE - 1] of Byte;
-  BytesRead: Integer;
-begin
-  ByteBuffer := 0;
-  ByteBufferSpace := 4;
-  BytesRead := InputStream.Read(InputBuffer, SizeOf(InputBuffer));
-  while BytesRead > 0 do
-  begin
-    OutputStream.Write(OutputBuffer, MimeDecodePartial(InputBuffer, BytesRead, OutputBuffer, ByteBuffer, ByteBufferSpace));
-    BytesRead := InputStream.Read(InputBuffer, SizeOf(InputBuffer));
-  end;
-  OutputStream.Write(OutputBuffer, MimeDecodePartialEnd(OutputBuffer, ByteBuffer, ByteBufferSpace));
-end;
-
-//--------------------------------------------------------------------------------------------------
-
 procedure DecodeHttpBasicAuthentication(const BasicCredentials: AnsiString; out UserId, PassWord: AnsiString);
 label
   Fail;
@@ -335,7 +308,7 @@ begin
   MimeEncodeNoCRLF(Pointer(Cardinal(@InputBuffer) + IDelta)^, InputByteCount - IDelta, Pointer(Cardinal(@OutputBuffer) + ODelta)^);
 end;
 
-{ ---------- }
+//--------------------------------------------------------------------------------------------------
 
 procedure MimeEncodeFullLines(const InputBuffer; const InputByteCount: Cardinal; out OutputBuffer);
 var
@@ -388,7 +361,7 @@ begin
   until InnerLimit > OuterLimit;
 end;
 
-{ ---------- }
+//--------------------------------------------------------------------------------------------------
 
 procedure MimeEncodeNoCRLF(const InputBuffer; const InputByteCount: Cardinal; out OutputBuffer);
 var
@@ -455,9 +428,9 @@ begin
   end;
 end;
 
-{ ---------------------------------------------------------------------------- }
-{ Decoding Core
-{ ---------------------------------------------------------------------------- }
+//--------------------------------------------------------------------------------------------------
+// Decoding Core
+//--------------------------------------------------------------------------------------------------
 
 function MimeDecode(const InputBuffer; const InputBytesCount: Cardinal; out OutputBuffer): Cardinal;
 var
@@ -469,7 +442,7 @@ begin
   Inc(Result, MimeDecodePartialEnd(Pointer(Cardinal(@OutputBuffer) + Result)^, ByteBuffer, ByteBufferSpace));
 end;
 
-{ ---------------------------------------------------------------------------- }
+//--------------------------------------------------------------------------------------------------
 
 function MimeDecodePartial(const InputBuffer; const InputBytesCount: Cardinal; out OutputBuffer; var ByteBuffer: Cardinal; var ByteBufferSpace: Cardinal): Cardinal;
 var
@@ -514,7 +487,7 @@ begin
     Result := 0;
 end;
 
-{ ---------------------------------------------------------------------------- }
+//--------------------------------------------------------------------------------------------------
 
 function MimeDecodePartialEnd(out OutputBuffer; const ByteBuffer: Cardinal; const ByteBufferSpace: Cardinal): Cardinal;
 var
@@ -540,9 +513,141 @@ begin
   end;
 end;
 
+//--------------------------------------------------------------------------------------------------
+// File Encoding & Decoding
+//--------------------------------------------------------------------------------------------------
+
+procedure MimeEncodeFile(const InputFileName, OutputFileName: AnsiString);
+var
+  InputStream, OutputStream: TFileStream;
+begin
+  InputStream := TFileStream.Create(InputFileName, fmOpenRead or fmShareDenyWrite);
+  try
+    OutputStream := TFileStream.Create(OutputFileName, fmCreate);
+    try
+      MimeEncodeStream(InputStream, OutputStream);
+    finally
+      OutputStream.Free;
+    end;
+  finally
+    InputStream.Free;
+  end;
+end;
+
+//--------------------------------------------------------------------------------------------------
+
+procedure MimeEncodeFileNoCRLF(const InputFileName, OutputFileName: AnsiString);
+var
+  InputStream, OutputStream: TFileStream;
+begin
+  InputStream := TFileStream.Create(InputFileName, fmOpenRead or fmShareDenyWrite);
+  try
+    OutputStream := TFileStream.Create(OutputFileName, fmCreate);
+    try
+      MimeEncodeStreamNoCRLF(InputStream, OutputStream);
+    finally
+      OutputStream.Free;
+    end;
+  finally
+    InputStream.Free;
+  end;
+end;
+
+//--------------------------------------------------------------------------------------------------
+
+procedure MimeDecodeFile(const InputFileName, OutputFileName: AnsiString);
+var
+  InputStream, OutputStream: TFileStream;
+begin
+  InputStream := TFileStream.Create(InputFileName, fmOpenRead or fmShareDenyWrite);
+  try
+    OutputStream := TFileStream.Create(OutputFileName, fmCreate);
+    try
+      MimeDecodeStream(InputStream, OutputStream);
+    finally
+      OutputStream.Free;
+    end;
+  finally
+    InputStream.Free;
+  end;
+end;
+
+//--------------------------------------------------------------------------------------------------
+// Stream Encoding & Decoding
+//--------------------------------------------------------------------------------------------------
+
+procedure MimeEncodeStream(const InputStream: TStream; const OutputStream: TStream);
+var
+  InputBuffer: array[0..MIME_BUFFER_SIZE - 1] of Byte;
+  OutputBuffer: array[0..(MIME_BUFFER_SIZE + 2) div 3 * 4 + MIME_BUFFER_SIZE div MIME_DECODED_LINE_BREAK * 2 - 1] of Byte;
+  BytesRead: Cardinal;
+  IDelta, ODelta: Cardinal;
+begin
+  BytesRead := InputStream.Read(InputBuffer, SizeOf(InputBuffer));
+
+  while BytesRead = SizeOf(InputBuffer) do
+    begin
+      MimeEncodeFullLines(InputBuffer, SizeOf(InputBuffer), OutputBuffer);
+      OutputStream.Write(OutputBuffer, SizeOf(OutputBuffer));
+      BytesRead := InputStream.Read(InputBuffer, SizeOf(InputBuffer));
+    end;
+
+  MimeEncodeFullLines(InputBuffer, BytesRead, OutputBuffer);
+
+  IDelta := BytesRead div MIME_DECODED_LINE_BREAK; // Number of lines processed.
+  ODelta := IDelta * (MIME_ENCODED_LINE_BREAK + 2);
+  IDelta := IDelta * MIME_DECODED_LINE_BREAK;
+  MimeEncodeNoCRLF(Pointer(Cardinal(@InputBuffer) + IDelta)^, BytesRead - IDelta, Pointer(Cardinal(@OutputBuffer) + ODelta)^);
+
+  OutputStream.Write(OutputBuffer, MimeEncodedSize(BytesRead));
+end;
+
+//--------------------------------------------------------------------------------------------------
+
+procedure MimeEncodeStreamNoCRLF(const InputStream: TStream; const OutputStream: TStream);
+var
+  InputBuffer: array[0..MIME_BUFFER_SIZE - 1] of Byte;
+  OutputBuffer: array[0..((MIME_BUFFER_SIZE + 2) div 3) * 4 - 1] of Byte;
+  BytesRead: Cardinal;
+begin
+  BytesRead := InputStream.Read(InputBuffer, SizeOf(InputBuffer));
+  while BytesRead = SizeOf(InputBuffer) do
+    begin
+      MimeEncodeNoCRLF(InputBuffer, SizeOf(InputBuffer), OutputBuffer);
+      OutputStream.Write(OutputBuffer, SizeOf(OutputBuffer));
+      BytesRead := InputStream.Read(InputBuffer, SizeOf(InputBuffer));
+    end;
+
+  MimeEncodeNoCRLF(InputBuffer, BytesRead, OutputBuffer);
+  OutputStream.Write(OutputBuffer, MimeEncodedSizeNoCRLF(BytesRead));
+end;
+
+//--------------------------------------------------------------------------------------------------
+
+procedure MimeDecodeStream(const InputStream: TStream; const OutputStream: TStream);
+var
+  ByteBuffer, ByteBufferSpace: Cardinal;
+  InputBuffer: array[0..MIME_BUFFER_SIZE - 1] of Byte;
+  OutputBuffer: array[0..(MIME_BUFFER_SIZE + 3) div 4 * 3 - 1] of Byte;
+  BytesRead: Cardinal;
+begin
+  ByteBuffer := 0;
+  ByteBufferSpace := 4;
+  BytesRead := InputStream.Read(InputBuffer, SizeOf(InputBuffer));
+  while BytesRead > 0 do
+    begin
+      OutputStream.Write(OutputBuffer, MimeDecodePartial(InputBuffer, BytesRead, OutputBuffer, ByteBuffer, ByteBufferSpace));
+      BytesRead := InputStream.Read(InputBuffer, SizeOf(InputBuffer));
+    end;
+  OutputStream.Write(OutputBuffer, MimeDecodePartialEnd(OutputBuffer, ByteBuffer, ByteBufferSpace));
+end;
+
 // History:
 
 // $Log$
+// Revision 1.8  2004/07/07 22:34:25  mthoma
+// Added Ralf MimeStreams and MimeFile utilities...
+//
 // Revision 1.7  2004/06/14 13:05:18  marquardt
 // style cleaning ENDIF, Tabs
 //
