c105feca88c4fd4ed370f4d8d00e9b169bfa881f
ahuser <ahuser@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-09-15 13:36:12 +0000

    added chmod a+x for jpp

--- install/prototypes/jpp.sh
+++ install/prototypes/jpp.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 #
 # shell script to generate installer units from prototypes
 #
@@ -11,6 +12,7 @@ CLXOPTIONS="-c -dVisualCLX -dHAS_UNIT_TYPES -uDevelop -uVCL -x../Q"
 VCLOPTIONS="-c -dVCL -dMSWINDOWS -uDevelop -uVisualCLX -uHAS_UNIT_LIBC -uUnix -uLinux -uKYLIX -x../"
 FILES="JclInstall.pas JediInstallIntf.pas ProductFrames.pas JediInstallerMain.pas"
 
+chmod a+x $JPP >/dev/null 2>/dev/null
 $JPP $CLXOPTIONS $FILES
 $JPP $VCLOPTIONS $FILES
 
