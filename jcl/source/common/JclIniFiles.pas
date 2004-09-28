da086cd7188cc63148ecc7b25840928b67836f39
rrossmair <rrossmair@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-09-28 14:22:01 +0000

    removed PH contributions

--- jcl/source/common/JclIniFiles.pas
+++ jcl/source/common/JclIniFiles.pas
@@ -18,7 +18,6 @@
 { Contributors:                                                                                    }
 {   Eric S. Fisher                                                                                 }
 {   John C Molyneux                                                                                }
-{   Peter J. Haas (peterjhaas)                                                                     }
 {   Petr Vones (pvones)                                                                            }
 {   Robert Marquardt (marquardt)                                                                   }
 {   Robert Rossmair (rrossmair)                                                                    }
@@ -54,38 +53,7 @@ procedure IniWriteString(const FileName, Section, Line, Value: string);         
 procedure IniReadStrings(IniFile: TCustomIniFile; const Section: string; Strings: TStrings);
 procedure IniWriteStrings(IniFile: TCustomIniFile; const Section: string; Strings: TStrings);
 
-//--------------------------------------------------------------------------------------------------
-// IniFile interface without localized texts
-//--------------------------------------------------------------------------------------------------
-
-type
-  TJclISOMemIniFile = class(TMemIniFile)
-  public
-    function ReadDate(const Section, Name: string; Default: TDateTime): TDateTime; override;
-    function ReadDateTime(const Section, Name: string; Default: TDateTime): TDateTime; override;
-    function ReadFloat(const Section, Name: string; Default: Double): Double; override;
-    function ReadTime(const Section, Name: string; Default: TDateTime): TDateTime; override;
-    procedure WriteDate(const Section, Name: string; Value: TDateTime); override;
-    procedure WriteDateTime(const Section, Name: string; Value: TDateTime); override;
-    procedure WriteFloat(const Section, Name: string; Value: Double); override;
-    procedure WriteTime(const Section, Name: string; Value: TDateTime); override;
-  end;
-
-  TJclISOIniFile = class(TIniFile)
-  public
-    function ReadDate(const Section, Name: string; Default: TDateTime): TDateTime; override;
-    function ReadDateTime(const Section, Name: string; Default: TDateTime): TDateTime; override;
-    function ReadFloat(const Section, Name: string; Default: Double): Double; override;
-    function ReadTime(const Section, Name: string; Default: TDateTime): TDateTime; override;
-    procedure WriteDate(const Section, Name: string; Value: TDateTime); override;
-    procedure WriteDateTime(const Section, Name: string; Value: TDateTime); override;
-    procedure WriteFloat(const Section, Name: string; Value: Double); override;
-    procedure WriteTime(const Section, Name: string; Value: TDateTime); override;
-  end;
-
 implementation
-uses
-  JclDITs;
 
 //==================================================================================================
 // Initialization Files
@@ -215,96 +183,12 @@ begin
   end;
 end;
 
-//--------------------------------------------------------------------------------------------------
-
-function TJclISOMemIniFile.ReadDate(const Section, Name: string; Default: TDateTime): TDateTime;
-begin
-  Result := ISOStrToDateDef(ReadString(Section, Name, ''), Default);
-end;
-
-function TJclISOMemIniFile.ReadDateTime(const Section, Name: string; Default: TDateTime): TDateTime;
-begin
-  Result := ISOStrToDateTimeDef(ReadString(Section, Name, ''), Default);
-end;
-
-function TJclISOMemIniFile.ReadFloat(const Section, Name: string; Default: Double): Double;
-begin
-  Result := ISOStrToFloatDef(ReadString(Section, Name, ''), Default);
-end;
-
-function TJclISOMemIniFile.ReadTime(const Section, Name: string; Default: TDateTime): TDateTime;
-begin
-  Result := ISOStrToTimeDef(ReadString(Section, Name, ''), Default);
-end;
-
-procedure TJclISOMemIniFile.WriteDate(const Section, Name: string; Value: TDateTime);
-begin
-  WriteString(Section, Name, ISODateToStr(Value));
-end;
-
-procedure TJclISOMemIniFile.WriteDateTime(const Section, Name: string; Value: TDateTime);
-begin
-  WriteString(Section, Name, ISODateTimeToStr(Value));
-end;
-
-procedure TJclISOMemIniFile.WriteFloat(const Section, Name: string; Value: Double);
-begin
-  WriteString(Section, Name, ISOFloatToStr(Value));
-end;
-
-procedure TJclISOMemIniFile.WriteTime(const Section, Name: string; Value: TDateTime);
-begin
-  WriteString(Section, Name, ISOTimeToStr(Value));
-end;
-
-//--------------------------------------------------------------------------------------------------
-
-function TJclISOIniFile.ReadDate(const Section, Name: string; Default: TDateTime): TDateTime;
-begin
-  Result := ISOStrToDateDef(ReadString(Section, Name, ''), Default);
-end;
-
-function TJclISOIniFile.ReadDateTime(const Section, Name: string; Default: TDateTime): TDateTime;
-begin
-  Result := ISOStrToDateTimeDef(ReadString(Section, Name, ''), Default);
-end;
-
-function TJclISOIniFile.ReadFloat(const Section, Name: string; Default: Double): Double;
-begin
-  Result := ISOStrToFloatDef(ReadString(Section, Name, ''), Default);
-end;
-
-function TJclISOIniFile.ReadTime(const Section, Name: string; Default: TDateTime): TDateTime;
-begin
-  Result := ISOStrToTimeDef(ReadString(Section, Name, ''), Default);
-end;
-
-procedure TJclISOIniFile.WriteDate(const Section, Name: string; Value: TDateTime);
-begin
-  WriteString(Section, Name, ISODateToStr(Value));
-end;
-
-procedure TJclISOIniFile.WriteDateTime(const Section, Name: string; Value: TDateTime);
-begin
-  WriteString(Section, Name, ISODateTimeToStr(Value));
-end;
-
-procedure TJclISOIniFile.WriteFloat(const Section, Name: string; Value: Double);
-begin
-  WriteString(Section, Name, ISOFloatToStr(Value));
-end;
-
-procedure TJclISOIniFile.WriteTime(const Section, Name: string; Value: TDateTime);
-begin
-  WriteString(Section, Name, ISOTimeToStr(Value));
-end;
-
 // History:
 
-// 2004-03-23, Peter J. Haas
-//  - add TJclISOMemIniFile, TJclISOIniFile
-
 // $Log$
+// Revision 1.8  2004/09/28 14:22:01  rrossmair
+// removed PH contributions
+//
 // Revision 1.7  2004/07/30 07:20:25  marquardt
 // fixing TStringLists, adding BeginUpdate/EndUpdate
 //
@@ -316,9 +200,6 @@ end;
 //
 // Revision 1.4  2004/05/05 00:04:11  mthoma
 // Updated headers: Added donors as contributors, adjusted the initial authors, added cvs names when they were not obvious. Changed $data to $date where necessary,
-//
-// Revision 1.3  2004/04/06 04:32:43  peterjhaas
-// Add TJclISOIniFile, TJclISOMemIniFile
 //
 
 end.
