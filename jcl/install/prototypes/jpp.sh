127ec7fd40c4a1efd50c6d2c11314cfeae94a6d6
rrossmair <rrossmair@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-11-11 15:36:58 +0000

    adapted to the following changes: - .\prototypes\JclInstall.pas moved to .\JclInstall.pas - .\prototypes\JediInstallIntf.pas moved to .\JediInstall.pas No need to run JPP on these anymore.

--- jcl/install/prototypes/jpp.sh
+++ jcl/install/prototypes/jpp.sh
@@ -10,7 +10,7 @@
 JPP=../../source/prototypes/jpp
 CLXOPTIONS="-c -dVisualCLX -dHAS_UNIT_TYPES -uDevelop -uVCL -x../Q"
 VCLOPTIONS="-c -dVCL -dMSWINDOWS -uDevelop -uVisualCLX -uHAS_UNIT_LIBC -uUnix -uLinux -uKYLIX -x../"
-FILES="JclInstall.pas JediInstallIntf.pas ProductFrames.pas JediInstallerMain.pas"
+FILES="ProductFrames.pas JediInstallerMain.pas"
 
 chmod a+x $JPP >/dev/null 2>/dev/null
 $JPP $CLXOPTIONS $FILES
