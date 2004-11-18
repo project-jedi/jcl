f004e5516b710a705e54d007cb44d6b51ecac24a
rrossmair <rrossmair@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-11-18 00:57:14 +0000

    - check-in for release 1.93

--- clean.bat
+++ clean.bat
@@ -1,5 +1,8 @@
 @echo cleaning...
+@REM do not delete precompiled installer
+@ren bin\*Installer.exe *Installer.ex_
 @del /f /s *.~* bin\*.exe bin\*.dll *.a *.bpi *.dcp *.dcu *.dpu *.hpp *.jdbg *.map *.o 
+@ren bin\*Installer.ex_ *Installer.exe
 @cd lib
 @del /f /s *.obj *.res
 @cd ..
