ff86b69789e320f7041712fb52ebbf356c65042b
rayspostbox3 <rayspostbox3@9a6cfd80-1415-5440-b600-b8f113e22350>
2004-12-05 19:36:51 +0000

    Additions to the test unit for testing TEDIObjectList.

--- qa/automated/dunit/units/TestJclEDI.pas
+++ qa/automated/dunit/units/TestJclEDI.pas
@@ -7,7 +7,17 @@ uses
   TestFrameWork, TestExtensions;
 
 type
-	TJclEDI_StringReplace_Tests = class(TTestCase)
+
+  TEDIObjectList_Tests = class(TTestCase)
+  private
+
+	published
+    procedure Comprehensive_001;
+    procedure Comprehensive_002;
+    procedure Comprehensive_003;    
+  end;
+
+	TEDIStringReplace_Tests = class(TTestCase)
 	private
 	  procedure DoTest(S1, OldPattern, NewPattern: string;
       Flags: TReplaceFlags = []);
@@ -62,7 +72,92 @@ const
 	TestData02 = 'abcdefghijklmno';
 	TestData03 = 'aaabbbcccdddeeeaaabbbcccdddeee';
 
-procedure TJclEDI_StringReplace_Tests.DoTest(S1, OldPattern, NewPattern: string;
+{ TEDIObjectList_Tests }
+{
+	Tests with items
+}
+procedure TEDIObjectList_Tests.Comprehensive_001;
+var
+  OL: TEDIObjectList;
+  LI, LI0, LI1, LI2, LI3, LI4, LI5: TEDIObjectListItem;
+begin
+  OL := TEDIObjectList.Create(True);
+  try
+    LI0 := TEDIObjectListItem.Create(nil, nil, TEDIObject.Create);
+    OL.Add(LI0, 'Element 1');
+    LI1 := TEDIObjectListItem.Create(nil, nil, TEDIObject.Create);
+    OL.Add(LI1, 'Element 2');
+    LI2 := TEDIObjectListItem.Create(nil, nil, TEDIObject.Create);
+    OL.Add(LI2, 'Element 3');
+    LI3 := TEDIObjectListItem.Create(nil, nil, TEDIObject.Create);
+    OL.Add(LI3, 'Element 4');
+    LI4 := TEDIObjectListItem.Create(nil, nil, TEDIObject.Create);
+    OL.Add(LI4, 'Element 5');
+    Check(OL.Count = 5, 'TEDIObjectList.Add failed.');
+    LI5 := OL.Find(LI2);
+    Check(LI5 = LI2, 'TEDIObjectList.Find failed.');
+    LI2 := OL.Extract(LI2);
+    Check(OL.Count = 4, 'TEDIObjectList.Extract failed.');
+    OL.Remove(LI1);
+    Check(OL.Count = 3, 'TEDIObjectList.Remove failed.');
+    LI2 := OL.Insert(LI2, LI3);
+    Check(OL.Count = 4, 'TEDIObjectList.Insert failed.');
+    LI1 := OL.Insert(LI2);
+    Check(OL.Count = 5, 'TEDIObjectList.Insert failed.');
+    LI := OL.First;
+    Check(LI = LI0, 'TEDIObjectList.First failed.');
+    LI := OL.Next;
+    Check(LI = LI1, 'TEDIObjectList.Next failed.');
+    LI := OL.Last;
+    Check(LI = LI4, 'TEDIObjectList.Last failed.');
+    LI := OL.Prior;
+    Check(LI = LI3, 'TEDIObjectList.Prior failed.');
+  finally
+    OL.Free;
+  end; //try
+end;
+{
+	Tests with objects
+}
+procedure TEDIObjectList_Tests.Comprehensive_002;
+var
+  OL: TEDIObjectList;
+  LI0, LI1, LI2, LI3, LI4, LI5: TEDIObjectListItem;
+  O: TEDIObject;
+begin
+  OL := TEDIObjectList.Create(True);
+  try
+    LI0 := OL.Add(TEDIObject.Create, 'Element 1');
+    LI1 := OL.Add(TEDIObject.Create, 'Element 2');
+    LI2 := OL.Add(TEDIObject.Create, 'Element 3');
+    LI3 := OL.Add(TEDIObject.Create, 'Element 4');
+    LI4 := OL.Add(TEDIObject.Create, 'Element 5');
+    Check(OL.Count = 5, 'TEDIObjectList.Add failed.');
+    LI5 := OL.Find(LI2.EDIObject);
+    Check(LI5 = LI2, 'TEDIObjectList.Find failed.');
+    O := OL.Extract(LI2.EDIObject);
+    Check(OL.Count = 4, 'TEDIObjectList.Extract failed.');
+    OL.Remove(LI1.EDIObject);
+    Check(OL.Count = 3, 'TEDIObjectList.Remove failed.');
+    LI2 := OL.Insert(O, LI3.EDIObject);
+    Check(OL.Count = 4, 'TEDIObjectList.Insert failed.');
+    LI1 := OL.Insert(LI2.EDIObject);
+    Check(OL.Count = 5, 'TEDIObjectList.Insert failed.');
+  finally
+    OL.Free;
+  end; //try
+end;
+{
+	Tests with dynamic array emulation
+}
+procedure TEDIObjectList_Tests.Comprehensive_003;
+begin
+
+end;
+
+{ TEDIStringReplace_Tests }
+
+procedure TEDIStringReplace_Tests.DoTest(S1, OldPattern, NewPattern: string;
 	Flags: TReplaceFlags);
 var
   S2, S3: string;
@@ -74,7 +169,7 @@ end;
 {
 	Replace a single character with a single character.
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_001;
+procedure TEDIStringReplace_Tests.StringReplace_001;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -89,7 +184,7 @@ end;
 {
 	Replace a single character with a string.
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_002;
+procedure TEDIStringReplace_Tests.StringReplace_002;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -104,7 +199,7 @@ end;
 {
 	Replace a string with a single character.
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_003;
+procedure TEDIStringReplace_Tests.StringReplace_003;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -119,7 +214,7 @@ end;
 {
 	Replace the first single character with a single character.
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_004;
+procedure TEDIStringReplace_Tests.StringReplace_004;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -134,7 +229,7 @@ end;
 {
 	Replace the first single character with a string
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_005;
+procedure TEDIStringReplace_Tests.StringReplace_005;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -149,7 +244,7 @@ end;
 {
 	Replace the first string with a single character
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_006;
+procedure TEDIStringReplace_Tests.StringReplace_006;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -164,7 +259,7 @@ end;
 {
 	Replace char pattern (beginning)
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_007;
+procedure TEDIStringReplace_Tests.StringReplace_007;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -179,7 +274,7 @@ end;
 {
 	Replace char pattern (middle)
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_008;
+procedure TEDIStringReplace_Tests.StringReplace_008;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -194,7 +289,7 @@ end;
 {
 	Replace char pattern (end)
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_009;
+procedure TEDIStringReplace_Tests.StringReplace_009;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -210,7 +305,7 @@ end;
 	Replace char pattern (beginning)
 	with string
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_010;
+procedure TEDIStringReplace_Tests.StringReplace_010;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -226,7 +321,7 @@ end;
 	Replace char pattern (middle)
 	with string
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_011;
+procedure TEDIStringReplace_Tests.StringReplace_011;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -242,7 +337,7 @@ end;
 	Replace char pattern (end)
 	with string
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_012;
+procedure TEDIStringReplace_Tests.StringReplace_012;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -258,7 +353,7 @@ end;
 	Replace string (beginning)
 	with char
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_013;
+procedure TEDIStringReplace_Tests.StringReplace_013;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -274,7 +369,7 @@ end;
 	Replace string (middle)
 	with char
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_014;
+procedure TEDIStringReplace_Tests.StringReplace_014;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -290,7 +385,7 @@ end;
 	Replace string (end)
 	with char
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_015;
+procedure TEDIStringReplace_Tests.StringReplace_015;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -305,7 +400,7 @@ end;
 {
 	Replace large string with small string (beginning)
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_016;
+procedure TEDIStringReplace_Tests.StringReplace_016;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -320,7 +415,7 @@ end;
 {
 	Replace large string with small string (middle)
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_017;
+procedure TEDIStringReplace_Tests.StringReplace_017;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -335,7 +430,7 @@ end;
 {
 	Replace large string with small string (end)
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_018;
+procedure TEDIStringReplace_Tests.StringReplace_018;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -350,7 +445,7 @@ end;
 {
 	Replace small string with large string (beginning)
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_019;
+procedure TEDIStringReplace_Tests.StringReplace_019;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -365,7 +460,7 @@ end;
 {
 	Replace small string with large string (middle)
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_020;
+procedure TEDIStringReplace_Tests.StringReplace_020;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -380,7 +475,7 @@ end;
 {
 	Replace small string with large string (end)
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_021;
+procedure TEDIStringReplace_Tests.StringReplace_021;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -395,7 +490,7 @@ end;
 {
 	Remove first char
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_022;
+procedure TEDIStringReplace_Tests.StringReplace_022;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -410,7 +505,7 @@ end;
 {
 	Remove middle char
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_023;
+procedure TEDIStringReplace_Tests.StringReplace_023;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -425,7 +520,7 @@ end;
 {
 	Remove last char
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_024;
+procedure TEDIStringReplace_Tests.StringReplace_024;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -440,7 +535,7 @@ end;
 {
 	Remove first string
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_025;
+procedure TEDIStringReplace_Tests.StringReplace_025;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -455,7 +550,7 @@ end;
 {
 	Remove middle string
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_026;
+procedure TEDIStringReplace_Tests.StringReplace_026;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -470,7 +565,7 @@ end;
 {
 	Remove last string
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_027;
+procedure TEDIStringReplace_Tests.StringReplace_027;
 var
 	OldPattern, NewPattern: string;
 	S1, S2, S3: string;
@@ -485,52 +580,56 @@ end;
 {
   Multi-string replacement tests
 }
-procedure TJclEDI_StringReplace_Tests.StringReplace_028;
+procedure TEDIStringReplace_Tests.StringReplace_028;
 begin
   DoTest(TestData01, 'aaa', 'zzz', [rfReplaceAll]);
 end;
 
-procedure TJclEDI_StringReplace_Tests.StringReplace_029;
+procedure TEDIStringReplace_Tests.StringReplace_029;
 begin
   DoTest(TestData01, 'ccc', 'zzz', [rfReplaceAll]);
 end;
 
-procedure TJclEDI_StringReplace_Tests.StringReplace_030;
+procedure TEDIStringReplace_Tests.StringReplace_030;
 begin
   DoTest(TestData01, 'eee', 'zzz', [rfReplaceAll]);
 end;
 
-procedure TJclEDI_StringReplace_Tests.StringReplace_031;
+procedure TEDIStringReplace_Tests.StringReplace_031;
 begin
   DoTest(TestData01, 'aaa', 'zz', [rfReplaceAll]);
 end;
 
-procedure TJclEDI_StringReplace_Tests.StringReplace_032;
+procedure TEDIStringReplace_Tests.StringReplace_032;
 begin
   DoTest(TestData01, 'ccc', 'zz', [rfReplaceAll]);
 end;
 
-procedure TJclEDI_StringReplace_Tests.StringReplace_033;
+procedure TEDIStringReplace_Tests.StringReplace_033;
 begin
   DoTest(TestData01, 'eee', 'zz', [rfReplaceAll]);
 end;
 
-procedure TJclEDI_StringReplace_Tests.StringReplace_034;
+procedure TEDIStringReplace_Tests.StringReplace_034;
 begin
   DoTest(TestData01, 'aaa', 'zzzzzz', [rfReplaceAll]);
 end;
 
-procedure TJclEDI_StringReplace_Tests.StringReplace_035;
+procedure TEDIStringReplace_Tests.StringReplace_035;
 begin
   DoTest(TestData01, 'ccc', 'zzzzzz', [rfReplaceAll]);
 end;
 
-procedure TJclEDI_StringReplace_Tests.StringReplace_036;
+procedure TEDIStringReplace_Tests.StringReplace_036;
 begin
   DoTest(TestData01, 'eee', 'zzzzzz', [rfReplaceAll]);
 end;
 
+
+
 initialization
- TestFramework.RegisterTest(TJclEDI_StringReplace_Tests.Suite);
+ TestFramework.RegisterTests('JclEDI',
+   [TEDIObjectList_Tests.Suite,
+    TEDIStringReplace_Tests.Suite]);
 
 end.
