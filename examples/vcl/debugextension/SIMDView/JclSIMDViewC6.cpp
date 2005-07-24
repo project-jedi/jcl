0839ee720203bdcea2bfa6dc2777987ec0edfa66
outchy <outchy@9a6cfd80-1415-5440-b600-b8f113e22350>
2005-07-24 16:23:03 +0000

    JclSIMDView can now be installed with the JCL installer or compile in the IDE. Added the icon.

--- examples/vcl/debugextension/SIMDView/JclSIMDViewC6.cpp
+++ examples/vcl/debugextension/SIMDView/JclSIMDViewC6.cpp
@@ -1,20 +1,20 @@
 //---------------------------------------------------------------------------
 
-#include <basepch.h>
+#include <vcl.h>
 #pragma hdrstop
-USEFORMNS("JclSIMDViewForm.pas", Jclsimdviewform, JvSIMDViewFrm);
-USEFORMNS("JclSIMDModifyForm.pas", Jclsimdmodifyform, JvSIMDModifyFrm);
-USEFORMNS("JclSIMDCpuInfo.pas", Jclsimdcpuinfo, FormCpuInfo);
+USEFORMNS("JclSIMDViewForm.pas", Jclsimdviewform, JclSIMDViewFrm);
+USEFORMNS("JclSIMDModifyForm.pas", Jclsimdmodifyform, JclSIMDModifyFrm);
+USEFORMNS("JclSIMDCpuInfo.pas", Jclsimdcpuinfo, JclFormCpuInfo);
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 //---------------------------------------------------------------------------
 
-//  Source du paquet.
+//   Source du paquet.
 //---------------------------------------------------------------------------
 
 #pragma argsused
 int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void*)
 {
-  return 1;
+        return 1;
 }
 //---------------------------------------------------------------------------
