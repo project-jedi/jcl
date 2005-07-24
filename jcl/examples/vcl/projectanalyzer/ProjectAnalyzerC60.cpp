6ebfc569760bc9602fba65b5dae9bb49256e9f90
outchy <outchy@9a6cfd80-1415-5440-b600-b8f113e22350>
2005-07-24 16:24:15 +0000

    ProjectAnalyser can now be installed using the JCL installer or compiled by the IDE.

--- jcl/examples/vcl/projectanalyzer/ProjectAnalyzerC60.cpp
+++ jcl/examples/vcl/projectanalyzer/ProjectAnalyzerC60.cpp
@@ -1,18 +1,18 @@
 //---------------------------------------------------------------------------
 
-#include <basepch.h>
+#include <vcl.h>
 #pragma hdrstop
 USEFORMNS("ProjAnalyzerFrm.pas", Projanalyzerfrm, ProjectAnalyzerForm);
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 //---------------------------------------------------------------------------
 
-//  Source du paquet.
+//   Source du paquet.
 //---------------------------------------------------------------------------
 
 #pragma argsused
 int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void*)
 {
-  return 1;
+        return 1;
 }
 //---------------------------------------------------------------------------
