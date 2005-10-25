c1b04dff0d995ab3d2e433c11aad379eb5593243
rrossmair <rrossmair@9a6cfd80-1415-5440-b600-b8f113e22350>
2005-10-25 07:11:04 +0000

    - moved over from jcl\examples\vcl\debug

---
+++ jcl/examples/vcl/debug/framestrack/FramesTrackExample.dpr
@@ -0,0 +1,13 @@
+program FramesTrackExample;
+
+uses
+  Forms,
+  FramesTrackDemoMain in 'FramesTrackDemoMain.pas' {Form1};
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
