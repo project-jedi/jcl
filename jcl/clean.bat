896dbc4785d8ab623d17a2300cfc8c0b89c57e95
outchy <outchy@9a6cfd80-1415-5440-b600-b8f113e22350>
2006-02-05 13:00:14 +0000

    Added extensions to be ignored and deleted

--- jcl/clean.bat
+++ jcl/clean.bat
@@ -1,9 +1,9 @@
 @echo cleaning...
 @REM do not delete precompiled installer
 @for %%f in (bin\*.exe) do @if not %%f==bin\JediInstaller.exe if not %%f==bin\QJediInstaller.exe (del %%f)
-@del /f /s *.~* *.bk bin\*.dll *.a *.bpi *.dcp *.dcu *.dpu *.hpp *.jdbg *.map *.o 
+@del /f /s *.~* *.bk bin\*.dll *.a *.bpi *.dcp *.dcu *.dpu *.hpp *.jdbg *.map *.o
 @cd lib
-@del /f /s *.obj *.res
+@del /f /s *.obj *.res *.lib *.bpi
 @cd ..
 @cd examples
 @del /f /s *.cfg
@@ -11,3 +11,6 @@
 @cd experts
 @del /f /s *.cfg
 @cd ..
+@cd packages
+@del /f /s *.cfg
+@cd..
