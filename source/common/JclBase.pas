9b6d06b25037e0357b22530cf4330c22142cdb8f
outchy <outchy@9a6cfd80-1415-5440-b600-b8f113e22350>
2006-12-20 17:24:47 +0000

    Introducing version 1.98. Identification of testing versions. Log file cleared before the installation starts.

--- source/common/JclBase.pas
+++ source/common/JclBase.pas
@@ -53,9 +53,9 @@ uses
 // Version
 const
   JclVersionMajor   = 1;    // 0=pre-release|beta/1, 2, ...=final
-  JclVersionMinor   = 97;   // Fifth minor release since JCL 1.90
-  JclVersionRelease = 1;    // 0: pre-release|beta/>=1: release
-  JclVersionBuild   = 2172; // build number, days since march 1, 2000
+  JclVersionMinor   = 98;   // Fifth minor release since JCL 1.90
+  JclVersionRelease = 0;    // 0: pre-release|beta/>=1: release
+  JclVersionBuild   = 2485; // build number, days since march 1, 2000
   JclVersion = (JclVersionMajor shl 24) or (JclVersionMinor shl 16) or
     (JclVersionRelease shl 15) or (JclVersionBuild shl 0);
 
