52003be2eb32a0d3e504b4ebba944dcdcb731245
outchy <outchy@9a6cfd80-1415-5440-b600-b8f113e22350>
2008-01-26 20:23:27 +0000

    JclUnicode was moved from source/windows to source/common.

--- thirdparty/unicode_data_extractor/make_jclunicode.bat
+++ thirdparty/unicode_data_extractor/make_jclunicode.bat
@@ -4,16 +4,16 @@ rem compile UDExtract.dpr
 dcc32 UDExtract.dpr -U..\..\jcl\source\windows;..\..\jcl\source\common -I..\..\jcl\source -N0. -E.
 
 rem execute UDExtract.dpr no compression
-UDExtract.exe UnicodeData.txt ..\..\jcl\source\windows\JclUnicode.rc /c=SpecialCasing.txt /f=CaseFolding.txt /d=DerivedNormalizationProps.txt
+UDExtract.exe UnicodeData.txt ..\..\jcl\source\common\JclUnicode.rc /c=SpecialCasing.txt /f=CaseFolding.txt /d=DerivedNormalizationProps.txt
 
 rem execute UDExtract.dpr zlib compression
-UDExtract.exe UnicodeData.txt ..\..\jcl\source\windows\JclUnicodeZLib.rc /z /c=SpecialCasing.txt /f=CaseFolding.txt /d=DerivedNormalizationProps.txt
+UDExtract.exe UnicodeData.txt ..\..\jcl\source\common\JclUnicodeZLib.rc /z /c=SpecialCasing.txt /f=CaseFolding.txt /d=DerivedNormalizationProps.txt
 
 rem execute UDExtract.dpr bzip2 compression
-UDExtract.exe UnicodeData.txt ..\..\jcl\source\windows\JclUnicodeBzip2.rc /bz /c=SpecialCasing.txt /f=CaseFolding.txt /d=DerivedNormalizationProps.txt
+UDExtract.exe UnicodeData.txt ..\..\jcl\source\common\JclUnicodeBzip2.rc /bz /c=SpecialCasing.txt /f=CaseFolding.txt /d=DerivedNormalizationProps.txt
 
 rem compiling JclUnicode.rc
-cd ..\..\jcl\source\windows
+cd ..\..\jcl\source\common
 brcc32 JclUnicode.rc -foJclUnicode.res
 brcc32 JclUnicodeZLib.rc -foJclUnicodeZLib.res
 brcc32 JclUnicodeBZip2.rc -foJclUnicodeBZip2.res
