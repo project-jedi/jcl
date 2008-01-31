664c1bbde38ac0bc99dc8df97d176dc8eabefa4d
marcovtje <marcovtje@9a6cfd80-1415-5440-b600-b8f113e22350>
2008-01-31 09:21:45 +0000

    * Fixed regression due to psid change with {IFDEF FPC}

--- jcl/source/windows/JclSecurity.pas
+++ jcl/source/windows/JclSecurity.pas
@@ -517,7 +517,7 @@ end;
 
 procedure StringToSID(const SIDString: String; SID: PSID; cbSID: DWORD);
 var
-  ASID: PSID;
+  {$ifdef FPC} ASID: PSID; {$else} ASID : ^_SID; {$ENDIF}
   CurrentPos, TempPos: Integer;
   AuthorityValue, RequiredSize: DWORD;
   Authority: string;
