96a7c00d4cffdf8219118228c44a84325681b26f
uschuster <uschuster@9a6cfd80-1415-5440-b600-b8f113e22350>
2009-04-29 22:41:47 +0000

    implemented StackCodeUtils.pas functions for the old IDE

--- jcl/experts/stackviewer/StackCodeUtils.pas
+++ jcl/experts/stackviewer/StackCodeUtils.pas
@@ -5,7 +5,13 @@ unit StackCodeUtils;
 interface
 
 uses
-  SysUtils, ActiveX, ToolsAPI, StackViewUnit;
+  SysUtils,
+  {$IFNDEF BDS}
+  Classes,
+  {$ENDIF !BDS}
+  ActiveX, ToolsAPI,
+  JclOtaUtils,
+  StackViewUnit;
 
 function FindModule(const AFileName: string): string;
 function FindModuleAndProject(const AFileName: string; var AProjectName: string): string;
@@ -14,13 +20,13 @@ procedure JumpToCode(AStackViewItem: TStackViewItem);
 
 implementation
 
-function FindModuleInProject(AProject: IOTAProject; const AFileName: string): string;
+function FindModuleInfoInProject(AProject: IOTAProject; const AFileName: string): IOTAModuleInfo;
 var
   I, P: Integer;
   ModuleInfo: IOTAModuleInfo;
   S, S2: string;
 begin
-  Result := '';
+  Result := nil;
   if AProject.GetModuleCount > 0 then
   begin
     S := UpperCase(AFileName);
@@ -33,7 +39,7 @@ begin
         P := Pos(S, S2);
         if (P > 0) and (P = Length(S2) - Length(S) + 1) then
         begin
-          Result := ModuleInfo.FileName;
+          Result := ModuleInfo;
           Break;
         end;
       end;
@@ -48,75 +54,131 @@ begin
   Result := FindModuleAndProject(AFilename, Dummy);
 end;
 
-function FindModuleAndProject(const AFileName: string; var AProjectName: string): string;
+function FindModuleInfoAndProject(const AFileName: string; var AProjectName: string): IOTAModuleInfo;
 var
   I: Integer;
   ProjectGroup: IOTAProjectGroup;
 begin
-  Result := '';
+  Result := nil;
   AProjectName := '';
   {$IFDEF BDS}
   ProjectGroup := (BorlandIDEServices as IOTAModuleServices).MainProjectGroup;
+  {$ELSE !BDS}
+  ProjectGroup := TJclOTAExpertBase.GetProjectGroup;
+  {$ENDIF !BDS}
   if Assigned(ProjectGroup) then
     for I := 0 to ProjectGroup.ProjectCount - 1 do
     begin
-      Result := FindModuleInProject(ProjectGroup.Projects[I], AFileName);
-      if Result <> '' then
+      Result := FindModuleInfoInProject(ProjectGroup.Projects[I], AFileName);
+      if Assigned(Result) then
       begin
         AProjectName := ProjectGroup.Projects[I].FileName;
         Break;
       end;
     end;
-  {$ENDIF BDS}
+end;
+
+function FindModuleAndProject(const AFileName: string; var AProjectName: string): string;
+var
+  ModuleInfo: IOTAModuleInfo;
+begin
+  ModuleInfo := FindModuleInfoAndProject(AFileName, AProjectName);
+  if Assigned(ModuleInfo) then
+    Result := ModuleInfo.FileName
+  else
+    Result := '';
 end;
 
 function GetFileEditorContent(const AFileName: string): IStream;
-{$IFDEF BDS}
 var
   I: Integer;
   Module: IOTAModule;
+  {$IFDEF BDS}
   EditorContent: IOTAEditorContent;
-{$ENDIF BDS}
+  {$ELSE !BDS}
+  ContentPos, ReadCount, BufferSize: Integer;
+  Buffer: Pointer;
+  ModuleSourceEditor: IOTASourceEditor;
+  ModuleReader: IOTAEditReader;
+  S: TStream;
+  SA: TStreamAdapter;
+  {$ENDIF !BDS}
 begin
   Result := nil;
-  {$IFDEF BDS}
   Module := (BorlandIDEServices as IOTAModuleServices).FindModule(AFileName);
   if Assigned(Module) then
   begin
+    {$IFDEF BDS}
     for I := 0 to Module.ModuleFileCount - 1 do
       if Supports(Module.ModuleFileEditors[I], IOTAEditorContent, EditorContent) then
       begin
         Result := EditorContent.Content;
         Break;
       end;
+    {$ELSE !BDS}
+    for I := 0 to Module.GetModuleFileCount - 1 do
+      if Supports(Module.GetModuleFileEditor(I), IOTASourceEditor, ModuleSourceEditor) then
+      begin
+        ModuleReader := ModuleSourceEditor.CreateReader;
+        if Assigned(ModuleReader) then
+        begin
+          ContentPos := 0;
+          BufferSize := 4096;
+          SA := TStreamAdapter.Create(TMemoryStream.Create, soOwned);
+          S := SA.Stream;
+          Result := SA;
+          GetMem(Buffer, BufferSize);
+          try
+            ReadCount := BufferSize;
+            while ReadCount = BufferSize do
+            begin
+              ReadCount := ModuleReader.GetText(ContentPos, Buffer, ReadCount);
+              if ReadCount > 0 then
+              begin
+                Inc(ContentPos, BufferSize);
+                S.Write(Buffer^, ReadCount);
+              end;
+            end;
+          finally
+            FreeMem(Buffer);
+          end;
+        end;
+        Break;
+      end;
+    {$ENDIF !BDS}
   end;
-  {$ENDIF BDS}
 end;
 
 procedure JumpToCode(AStackViewItem: TStackViewItem);
-{$IFDEF BDS}
 var
-  S, FileName: string;
+  S, FileName, ProjectName: string;
   Module: IOTAModule;
+  ModuleInfo: IOTAModuleInfo;  
 
   SourceEditor: IOTASourceEditor;
   I, LineNumber: Integer;
   EditPos: TOTAEditPos;
-{$ENDIF BDS}  
 begin
-  {$IFDEF BDS}
   if Assigned(AStackViewItem) then
   begin
     FileName := AStackViewItem.SourceName;
-    S := FindModule(FileName);
+    ModuleInfo := FindModuleInfoAndProject(FileName, ProjectName);
+    if Assigned(ModuleInfo) then
+      S := ModuleInfo.FileName;
     if (S <> '') and Assigned(BorlandIDEServices) then
     begin
+      {$IFDEF BDS}
       Module := (BorlandIDEServices as IOTAModuleServices).OpenModule(S);
+      {$ELSE !BDS}
+      Module := ModuleInfo.OpenModule;
+      {$ENDIF !BDS}
       if Assigned(Module) then
       begin
+        {$IFDEF BDS}
         Module.Show;
-        for I := 0 to Module.ModuleFileCount - 1 do
-          if Supports(Module.ModuleFileEditors[I], IOTASourceEditor, SourceEditor) then
+        {$ENDIF BDS}
+        for I := 0 to Module.GetModuleFileCount - 1 do
+          if Supports(Module.GetModuleFileEditor(I), IOTASourceEditor, SourceEditor) then
           begin
             SourceEditor.Show;
             if SourceEditor.EditViewCount > 0 then
@@ -138,7 +200,6 @@ begin
       end;
     end;
   end;
-  {$ENDIF BDS}
 end;
 
 end.
