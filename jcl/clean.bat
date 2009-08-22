718c14b63faf1ba2d4b6c48a859eacb64e37a19e
outchy <outchy@9a6cfd80-1415-5440-b600-b8f113e22350>
2009-08-22 04:27:55 +0000

    Kylix is not supported anymore.

--- jcl/clean.bat
+++ jcl/clean.bat
@@ -1,7 +1,7 @@
 @echo cleaning...
 @REM do not delete precompiled installer
-@for %%f in (bin\*.exe) do @if not %%f==bin\JediInstaller.exe if not %%f==bin\QJediInstaller.exe (del %%f)
-@del /f /s *.~* *.bk bin\*.dll *.a *.bpi *.dcp *.dcu *.dpu *.hpp *.jdbg *.map *.o
+@for %%f in (bin\*.exe) do @if not %%f==bin\JediInstaller.exe (del %%f)
+@del /f /s *.~* *.bk bin\*.dll *.a *.bpi *.dcp *.dcu *.hpp *.jdbg *.map *.o
 @cd lib
 @del /f /s *.obj *.res *.lib *.bpi
 @cd ..
