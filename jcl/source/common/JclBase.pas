814a171ef200022ea9a1554934be05ddb06af2c4
outchy <outchy@9a6cfd80-1415-5440-b600-b8f113e22350>
2009-11-05 19:11:46 +0000

    version numbers for JCL 2.1.1.3536.

--- jcl/source/common/JclBase.pas
+++ jcl/source/common/JclBase.pas
@@ -55,8 +55,8 @@ uses
 const
   JclVersionMajor   = 2;    // 0=pre-release|beta/1, 2, ...=final
   JclVersionMinor   = 1;    // Fifth minor release since JCL 1.90
-  JclVersionRelease = 0;    // 0: pre-release|beta/ 1: release
-  JclVersionBuild   = 3450; // build number, days since march 1, 2000
+  JclVersionRelease = 1;    // 0: pre-release|beta/ 1: release
+  JclVersionBuild   = 3536; // build number, days since march 1, 2000
   JclVersion = (JclVersionMajor shl 24) or (JclVersionMinor shl 16) or
     (JclVersionRelease shl 15) or (JclVersionBuild shl 0);
 
