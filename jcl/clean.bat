569d35ba16da3901df0c61f85c1ea21d54d945c8
outchy <outchy@9a6cfd80-1415-5440-b600-b8f113e22350>
2010-01-25 13:18:41 +0000

    Lazarus generates .bak files for backup.

--- jcl/clean.bat
+++ jcl/clean.bat
@@ -1,7 +1,7 @@
 @echo cleaning...
 @REM do not delete precompiled installer
 @for %%f in (bin\*.exe) do @if not %%f==bin\JediInstaller.exe (del %%f)
-@del /f /s *.~* *.bk bin\*.dll *.a *.bpi *.dcp *.dcu *.hpp *.jdbg *.map *.o
+@del /f /s *.~* *.bak *.bk bin\*.dll *.a *.bpi *.dcp *.dcu *.hpp *.jdbg *.map *.o
 @cd lib
 @del /f /s *.obj *.res *.lib *.bpi
 @cd ..
--- jcl/clean.sh
+++ jcl/clean.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rm -f `find -name \*~`
+rm -f `find -name *.bk`
+rm -f `find -name *.bak`
 rm -f `find -name *.~*`
 rm -f `find -name *.a`
 rm -f `find -name *.bpi`
