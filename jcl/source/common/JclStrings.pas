e733c3f40ab61d64f38afc4bf0716f1badc1ca24
jfudickar <jfudickar@9a6cfd80-1415-5440-b600-b8f113e22350>
2012-01-02 21:47:40 +0000

    Function StrSplit added

--- jcl/source/common/JclStrings.pas
+++ jcl/source/common/JclStrings.pas
@@ -278,13 +278,24 @@ function StrSearch(const Substr, S: string; const Index: SizeInt = 1): SizeInt;
 function StrSuffixIndex(const S: string; const Suffixes: array of string): SizeInt;
 
 // String Extraction
+// Returns the String before SubStr
 function StrAfter(const SubStr, S: string): string;
+/// Returns the string after SubStr
 function StrBefore(const SubStr, S: string): string;
+/// Splits a string at SubStr, returns true when SubStr is found, Left contains the
+/// string before the SubStr and Rigth the string behind SubStr
+function StrSplit(const SubStr, S: string;var Left, Right : string): boolean;
+/// Returns the string between Start and Stop
 function StrBetween(const S: string; const Start, Stop: Char): string;
+/// Returns the left N characters of the string
 function StrChopRight(const S: string; N: SizeInt): string;
+/// Returns the left Count characters of the string
 function StrLeft(const S: string; Count: SizeInt): string;
+/// Returns the string starting from position Start for the Count Characters
 function StrMid(const S: string; Start, Count: SizeInt): string;
+/// Returns the string starting from position N to the end
 function StrRestOf(const S: string; N: SizeInt): string;
+/// Returns the right Count characters of the string
 function StrRight(const S: string; Count: SizeInt): string;
 
 // Character Test Routines
@@ -2715,6 +2726,23 @@ begin
     Result := StrLeft(S, P - 1);
 end;
 
+function StrSplit(const SubStr, S: string;var Left, Right : string): boolean;
+var
+  P: SizeInt;
+begin
+  P := StrFind(SubStr, S, 1);
+  Result:= p > 0;
+  if Result then
+  begin
+    Left := StrLeft(S, P - 1);
+    Right := StrRestOf(S, P + Length(SubStr));
+  end
+  else
+  begin
+    Left := '';
+    Right := '';
+  end;
+end;
 
 function StrBetween(const S: string; const Start, Stop: Char): string;
 var
