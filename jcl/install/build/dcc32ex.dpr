46284e787caf0bdf86b99846de248cfabe988ad9
Andreas Hausladen <Andreas.Hausladen@gmail.com>
2013-05-13 21:59:31 +0200

    Made "XE" IDE name detection more generic

--- jcl/install/build/dcc32ex.dpr
+++ jcl/install/build/dcc32ex.dpr
@@ -558,9 +558,9 @@ begin
           6: Result.Name := 'CodeGear RAD Studio 2009';
           7: Result.Name := 'Embarcadero RAD Studio 2010';
           8: Result.Name := 'Embarcadero RAD Studio XE';
-          9: Result.Name := 'Embarcadero RAD Studio XE2';
-         10: Result.Name := 'Embarcadero RAD Studio XE3';
-        end;
+       else
+         Result.Name := 'Embarcadero RAD Studio XE' + IntToStr(2 + (Result.IDEVersion - 9));
+       end;
     end;
 
     if (Result.Typ = ttBDS) and (Result.IDEVersion >= 8) then
@@ -1151,7 +1151,6 @@ begin
     WriteLn(f, '-LUrtl');
   if RuntimePackageVcl then
     WriteLn(f, '-LUvcl');
-  {$MESSAGE HINT '-ns fix for Pulsar Beta 4 is quick and dirty (not sure if this is the best solution)'}
   if Target.Version >= 16 then
     WriteLn(f, '-nsSystem;System.Win;WinAPI;Vcl;Vcl.Imaging');
   CloseFile(f);
@@ -1200,7 +1199,7 @@ begin
   begin
     WriteLn;
     WriteLn('Additional options (must be specified before any dcc32 parameter):');
-    WriteLn('  --delphi-version=d17   Prefer this version, overrides environment variable');
+    WriteLn('  --delphi-version=d18   Prefer this version, overrides environment variable');
     WriteLn('  --verbose              Show warnings and errors during the compiler detection');
     WriteLn('  --use-search-paths     Use the IDE''s search paths');
     WriteLn('  --preserve-config      Keep the dcc32.cfg file and create a dcc32_command.cmd');
@@ -1212,8 +1211,8 @@ begin
     WriteLn('  --runtime-package-vcl  Link the executable against the vcl package');
     WriteLn;
     WriteLn('Environment variables:');
-    WriteLn('  DELPHIVERSION = d17    Prefer this Delphi/BCB/BDS version');
-    WriteLn('                         (d6, d7, c6, d9, d10, d11, d12, d14, d15, d16, d17, ...)');
+    WriteLn('  DELPHIVERSION = d18    Prefer this Delphi/BCB/BDS version');
+    WriteLn('                         (d6, d7, c6, d9, d10, d11, d12, d14, ..., d18, ...)');
   end;
 
   ExitCode := Status;
