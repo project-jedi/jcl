a2a033bf1b5c177718e6f3d8438779f7f85d2ade
Andreas Hausladen <Andreas.Hausladen@gmail.com>
2013-06-01 19:43:00 +0200

    Install.bat automatically updates/initializes the project-jedi submodule if git is available

--- jcl/Install.bat
+++ jcl/Install.bat
@@ -1,6 +1,31 @@
+@echo off
+
 SETLOCAL
 pushd "%~dp0"
 
+if not exist "..\.git" goto StartInstall
+::if exist "source\include\jedi\jedi.inc" goto StartInstall
+
+:: Check if git if available
+call git --version 2>NUL >NUL
+if ERRORLEVEL 1 goto CannotInitializeSubModules
+:: Initialize git submodules
+echo Initializing/Updating git submodules...
+pushd .
+cd ..
+call git submodule update --init
+if ERRORLEVEL 1 goto CannotInitializeSubModules
+popd
+goto StartInstall
+
+:CannotInitializeSubModules
+if exist "source\include\jedi\jedi.inc" goto StartInstall
+echo.
+echo The jcl\source\include\jedi git submodule can't be initialized. jedi.inc not found.
+echo.
+goto FailedCompile
+
+:StartInstall
 SET DELPHIVERSION=%1
 
 cd install
