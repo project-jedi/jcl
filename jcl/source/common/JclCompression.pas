b347b44ac43591b37357d47372bdd8611af51358
Andreas Hausladen <Andreas.Hausladen@gmail.com>
2015-07-23 23:34:15 +0200

    Fixed Mantis #6421: JclCompression: Wrong type declaration (Crash on x64 with bound check enabled)

--- jcl/source/common/JclCompression.pas
+++ jcl/source/common/JclCompression.pas
@@ -4285,7 +4285,7 @@ function TJclCompressionItem.UpdateFileTimes: Boolean;
 const
   FILE_WRITE_ATTRIBUTES = $00000100;
 var
-  FileHandle: HFILE;
+  FileHandle: THandle;
   ACreationTime, ALastAccessTime, ALastWriteTime: PFileTime;
 begin
   ReleaseStream;
