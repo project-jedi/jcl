a23c9d4f3f117ff565f462447a855783a861ceb0
obones <obones@free.fr>
2015-09-15 11:03:05 +0200

    Today is 5676

--- jcl/source/common/JclBase.pas
+++ jcl/source/common/JclBase.pas
@@ -63,7 +63,7 @@ const
   JclVersionMajor   = 2;    // 0=pre-release|beta/1, 2, ...=final
   JclVersionMinor   = 7;    // Fifth minor release since JCL 1.90
   JclVersionRelease = 0;    // 0: pre-release|beta/ 1: release
-  JclVersionBuild   = 5300; // build number, days since march 1, 2000
+  JclVersionBuild   = 5676; // build number, days since march 1, 2000
   JclVersion = (JclVersionMajor shl 24) or (JclVersionMinor shl 16) or
     (JclVersionRelease shl 15) or (JclVersionBuild shl 0);
 
