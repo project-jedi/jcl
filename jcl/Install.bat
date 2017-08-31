416bc6002a4b6554c01d3f0ca5add4351e14f4eb
Andreas Hausladen <Andreas.Hausladen@gmail.com>
2017-08-31 21:01:48 +0200

    Remove the -w command line option.

--- jcl/Install.bat
+++ jcl/Install.bat
@@ -60,7 +60,7 @@ build\dcc32ex.exe -Q -B -E..\bin build\JCLCmdStarter.dpr >NUL 2>NUL
 echo.
 echo ===================================================================
 echo Compiling JediInstaller...
-build\dcc32ex.exe %INSTALL_VERBOSE% --runtime-package-rtl --runtime-package-vcl -q -w -dJCLINSTALL -E..\bin -I..\source\include -U..\source\common;..\source\windows JediInstaller.dpr
+build\dcc32ex.exe %INSTALL_VERBOSE% --runtime-package-rtl --runtime-package-vcl -q -dJCLINSTALL -E..\bin -I..\source\include -U..\source\common;..\source\windows JediInstaller.dpr
 if ERRORLEVEL 1 goto FailedCompile
 :: New Delphi versions output "This product doesn't support command line compiling" and then exit with ERRORLEVEL 0
 if not exist ..\bin\JediInstaller.exe goto FailedCompile
