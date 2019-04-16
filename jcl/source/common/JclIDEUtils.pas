a7f54db4ab5b036a6a3a4632e1fc729adfc3fd00
mhumm <markus.humm@gmail.com>
2019-04-16 21:56:32 +0200

    Suggested fix for getting the correct registry key for XE2 C++ and up as of Mantis issue 6394

--- jcl/source/common/JclIDEUtils.pas
+++ jcl/source/common/JclIDEUtils.pas
@@ -987,6 +987,7 @@ const
 
   CppPathsKeyName            = 'CppPaths';
   CppPathsV5UpperKeyName     = 'C++\Paths';
+  CppPathsV9UpperKeyName     = 'C++\Paths\Win32';
   CppBrowsingPathValueName   = 'BrowsingPath';
   CppSearchPathValueName     = 'SearchPath';
   CppLibraryPathValueName    = 'LibraryPath';
@@ -3689,7 +3690,12 @@ end;
 function TJclBDSInstallation.GetCppPathsKeyName: string;
 begin
   if IDEVersionNumber >= 5 then
-    Result := CppPathsV5UpperKeyName
+  begin
+    if IDEVersionNumber >= 9 then
+      Result := CppPathsV9UpperKeyName
+    else
+      Result := CppPathsV5UpperKeyName
+  end
   else
     Result := CppPathsKeyName;
 end;
