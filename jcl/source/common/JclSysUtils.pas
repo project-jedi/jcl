2c67831aaf738f0ca1420a034e9cca1c30e1ff21
Artic-Circuits <25774804+Arctic-Circuits@users.noreply.github.com>
2025-01-10 12:17:59 -0500

    Update JclSysUtils.pas
    Change himself to themself

--- jcl/source/common/JclSysUtils.pas
+++ jcl/source/common/JclSysUtils.pas
@@ -2380,7 +2380,7 @@ begin
   FSignChars[False] := '-';
   FSignChars[True] := '+';
   FPaddingChar := ' ';
-  FMultiplier := '×';
+  FMultiplier := 'Ã—';
   FFractionalPartSeparator := JclFormatSettings.DecimalSeparator;
   FDigitBlockSeparator := JclFormatSettings.ThousandSeparator;
 end;
@@ -4528,7 +4528,7 @@ finalization
   {$ENDIF UNITVERSIONING}
   {$IFDEF MSWINDOWS}
   {$IFDEF THREADSAFE}
-  // The user must release shared memory blocks himself. We don't clean up their
+  // The user must release shared memory blocks themself. We don't clean up their
   // memory leaks and make it impossible to release the shared memory in other
   // unit's finalization blocks.
   MMFFinalized := True;
